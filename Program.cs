d26c5eafe9422279bb9c48f623ba933681dcabce
maurosil015 <maurosil015@gmail.com>
2023-10-25 21:08:37 +0200

    primera subida

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace primer_programa
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            int n1, n2, resultado;
+
+            Console.WriteLine("Ingrese un numero:");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese otro numero:");
+            n2 = int.Parse(Console.ReadLine());
+
+            resultado = n1 + n2;
+
+            Console.WriteLine("El resultado es: " + resultado);
+         }
+    }
+}
